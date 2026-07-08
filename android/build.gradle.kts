416711af1be76dfd436c2e19a2ee69789985723f
ievgen <ievgen@brigadesociety.com>
2026-07-08 13:00:13 +0300

    fixed kotlin version

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:4.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$2.2.20")
     }
 }
 
