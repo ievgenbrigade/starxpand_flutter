ad53dea17687149785abb9de4cf39b573840ad66
ievgen <ievgen@brigadesociety.com>
2026-07-15 20:10:18 +0300

    reverted version for plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,7 +2,7 @@ group = "com.ovatu.starxpand"
 version = "1.0-SNAPSHOT"
 
 plugins {
-    id("com.android.library") version "9.0.0" apply false
+    id("com.android.library")
 }
 
 rootProject.allprojects {
