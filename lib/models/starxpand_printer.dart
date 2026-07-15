4a163a9ababeb7f57e046d8cbee8f706bd36d9b2
ievgen <ievgen@brigadesociety.com>
2026-07-15 19:56:36 +0300

    changed paper api

--- lib/models/starxpand_printer.dart
+++ lib/models/starxpand_printer.dart
@@ -9,42 +9,42 @@ enum StarXpandInterface {
 }
 
 enum StarXpandPrinterPaper {
-  mm58('58mm'), // 385
-  mm76('76mm'), // 530
-  mm80('80mm'), // 567
-  mm112('112mm');
+  mm58('58mm', 58), // 385
+  mm76('76mm', 76), // 530
+  mm80('80mm', 80), // 567
+  mm112('112mm', 112);
 
   final String label;
+  final double value;
 
-  const StarXpandPrinterPaper(this.label);
+  const StarXpandPrinterPaper(this.label, this.value);
 }
 
 enum StarXpandPrinterModel {
-  tsp650II('TSP 650 II', [StarXpandPrinterPaper.mm80]),
-  tsp700II('TSP 600 II', [StarXpandPrinterPaper.mm80]),
-  tsp800II('TSP 800 II', [StarXpandPrinterPaper.mm80]),
-  tsp100IIUPlus('TSP 100 II U+', [StarXpandPrinterPaper.mm80]),
-  tsp100IIIW('TSP 100 III W', [StarXpandPrinterPaper.mm80]),
-  tsp100IIILAN('TSP 100 III LAN', [StarXpandPrinterPaper.mm80]),
-  tsp100IIIBI('TSP 100 III BI', [StarXpandPrinterPaper.mm80]),
-  tsp100IIIU('TSP 100 III U', [StarXpandPrinterPaper.mm80]),
-  tsp100IV('TSP 800II', [StarXpandPrinterPaper.mm80]),
-  mPOP('mPOP', [StarXpandPrinterPaper.mm58]),
-  mCPrint2('mC-Print2', [StarXpandPrinterPaper.mm58]),
-  mCPrint3('mC-Print3', [StarXpandPrinterPaper.mm80]),
-  smS210i('SM-S210i', [StarXpandPrinterPaper.mm58]),
-  smS230i('SM-S230i', [StarXpandPrinterPaper.mm58]),
-  smT300('SM-T300', [StarXpandPrinterPaper.mm80]),
-  smT300i('SM-T300i', [StarXpandPrinterPaper.mm80]),
-  smT400i('SM-T400i', [StarXpandPrinterPaper.mm112]),
-  smL200('SM-L200', [StarXpandPrinterPaper.mm58]),
-  smL300('SM-L300', [StarXpandPrinterPaper.mm58]),
-  sp700('SP-700', [StarXpandPrinterPaper.mm76]),
-  generic('Generic', [StarXpandPrinterPaper.mm80]),
-  unknown('Unknown', []);
+  tsp650II('TSP 650 II', StarXpandPrinterPaper.mm80),
+  tsp700II('TSP 600 II', StarXpandPrinterPaper.mm80),
+  tsp800II('TSP 800 II', StarXpandPrinterPaper.mm80),
+  tsp100IIUPlus('TSP 100 II U+', StarXpandPrinterPaper.mm80),
+  tsp100IIIW('TSP 100 III W', StarXpandPrinterPaper.mm80),
+  tsp100IIILAN('TSP 100 III LAN', StarXpandPrinterPaper.mm80),
+  tsp100IIIBI('TSP 100 III BI', StarXpandPrinterPaper.mm80),
+  tsp100IIIU('TSP 100 III U', StarXpandPrinterPaper.mm80),
+  tsp100IV('TSP 800II', StarXpandPrinterPaper.mm80),
+  mPOP('mPOP', StarXpandPrinterPaper.mm58),
+  mCPrint2('mC-Print2', StarXpandPrinterPaper.mm58),
+  mCPrint3('mC-Print3', StarXpandPrinterPaper.mm80),
+  smS210i('SM-S210i', StarXpandPrinterPaper.mm58),
+  smS230i('SM-S230i', StarXpandPrinterPaper.mm58),
+  smT300('SM-T300', StarXpandPrinterPaper.mm80),
+  smT300i('SM-T300i', StarXpandPrinterPaper.mm80),
+  smT400i('SM-T400i', StarXpandPrinterPaper.mm112),
+  smL200('SM-L200', StarXpandPrinterPaper.mm58),
+  smL300('SM-L300', StarXpandPrinterPaper.mm58),
+  sp700('SP-700', StarXpandPrinterPaper.mm76),
+  generic('Generic', StarXpandPrinterPaper.mm80);
 
   final String label;
-  final List<StarXpandPrinterPaper> paper;
+  final StarXpandPrinterPaper paper;
 
   const StarXpandPrinterModel(this.label, this.paper);
   factory StarXpandPrinterModel.fromName(String name) {
